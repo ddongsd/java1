547505c077ab18a1459f5bfe24660b07fd6b16fd
asusk <asusk@Mamoray-OE230HA>
2017-10-17 19:10:52 +0800

    cc

--- javaswing/src/com/sun/tu/SwingDemo.java
+++ javaswing/src/com/sun/tu/SwingDemo.java
@@ -1,17 +1,51 @@
 package com.sun.tu;
 
 import java.awt.*;
-
+import java.io.Console;
 import javax.swing.*;
 public class SwingDemo extends JFrame{
     public static void main(String[] args) {
        new SwingDemo().creat();
     }
     private void creat()
-    
     {
         Dimension d=Toolkit.getDefaultToolkit().getScreenSize();
         this.setBounds((int)(d.getWidth()/4),(int)(d.getHeight()/4),(int)(d.getWidth()/2), (int)(d.getHeight()/2));
         
     }
 }
+
+
+class CoCo {
+
+    public static void main(String[] args) {
+        Console console = System.console();  
+        String password ;  
+        password = new String(console.readPassword());  
+        System.out.println("password="+password); 
+        
+        String[][] temp=new String[99][];
+        for(int i=0;i<9;i++)
+        {
+            temp[i]=new String[3];
+            for(int j=0;j<2;j++)
+            {
+                temp[i][j]=new String("yuyuWW");
+            }
+        }
+        for(int i=0;i<19;i++)
+        {
+            if(temp[i]!=null)
+                System.out.println(temp[i].length); 
+            else
+            {
+                System.out.println(i);
+                break;
+            }
+        }
+       
+
+    }
+
+}
+
